b75ad94f08e8cefaa178421c072889b914a09ef0
Tymonoman <tymon.wojciechowicz@gmail.com>
2024-01-11 21:51:35 +0100

    added the GBP currency

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Replace 'YOUR_API_KEY' with your actual API key
 const apiKey = '8c863104fc53446aa859a1b06dccf539';
 const baseCurrency = 'USD';
-const targetCurrencies = ['EUR', 'PLN'];
+const targetCurrencies = ['EUR', 'PLN', 'GBP'];
 
 const apiUrl = `https://open.er-api.com/v6/latest/${baseCurrency}?apikey=${apiKey}`;
 
@@ -26,29 +26,39 @@ function updateCurrencies(amount, baseCurrency) {
     const usdInput = document.getElementById('usd');
     const eurInput = document.getElementById('eur');
     const plnInput = document.getElementById('pln');
+    const gbpInput = document.getElementById('gbp');
 
     ratesPromise.then(rates => {
         const usdRate = rates['USD'];
         const eurRate = rates['EUR'];
         const plnRate = rates['PLN'];
+        const gbpRate = rates['GBP'];
 
-        let usdValue, eurValue, plnValue;
+        let usdValue, eurValue, plnValue, gbpValue;
 
         switch (baseCurrency) {
             case 'USD':
                 usdValue = amount;
                 eurValue = amount * (eurRate / usdRate);
                 plnValue = amount * (plnRate / usdRate);
+                gbpValue = amount * (gbpRate / usdRate);
                 break;
             case 'EUR':
                 usdValue = amount * (usdRate / eurRate);
                 eurValue = amount;
                 plnValue = amount * (plnRate / eurRate);
+                gbpValue = amount * (gbpRate / eurRate);
                 break;
             case 'PLN':
                 usdValue = amount * (usdRate / plnRate);
                 eurValue = amount * (eurRate / plnRate);
                 plnValue = amount;
+                gbpValue = amount * (gbpRate / plnRate);
+            case 'GBP':
+                usdValue = amount * (usdRate / gbpRate);
+                eurValue = amount * (eurRate / gbpRate);
+                plnValue = amount * (plnRate / gbpRate);
+                gbpValue = amount; // Add this line
                 break;
             default:
                 break;
@@ -63,6 +73,10 @@ function updateCurrencies(amount, baseCurrency) {
         if (document.activeElement !== plnInput) {
             plnInput.value = plnValue.toFixed(2);
         }
+        if (document.activeElement !== gbpInput) {
+            gbpInput.value = gbpValue.toFixed(2);
+        }
+        
     });
 }
 
@@ -85,6 +99,13 @@ document.getElementById('pln').addEventListener('input', function (event) {
     const amount = parseFloat(event.target.value);
     if (!isNaN(amount)) {
         updateCurrencies(amount, 'PLN');
+    }
+});
+
+document.getElementById('gbp').addEventListener('input', function (event) {
+    const amount = parseFloat(event.target.value);
+    if (!isNaN(amount)) {
+        updateCurrencies(amount, 'GBP'); // Add this line
     }
 });
 
